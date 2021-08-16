470c63f749223b9378eddd42067a47d87db20556
Paddy Curé <paddy.cure@gmail.com>
2021-08-16 12:59:37 +0100

    added airdrop tokens

--- src/pages/Airdrops.js
+++ src/pages/Airdrops.js
@@ -191,7 +191,23 @@ const SubMessage = styled.div`
 
 export default function Airdrops({ connection }) {
 
-  const FSN_FUSE = "0xe96ac326ecea1a09ae6e47487c5d8717f73d5a7e"
+  const airdropTokens = [
+    {address: "0xe96ac326ecea1a09ae6e47487c5d8717f73d5a7e", symbol: "FSN/FUSE"},
+    {address: "0xd713b42a1695d5afe40eb8d203c285e0444b12e4", symbol: "FSN/BTC"},
+    {address: "0x6a69b46e072a0c9fb8c7c08bd70aaedcc0211782", symbol: "FSN/ETH"},
+    {address: "0x7ba62ccb1d4eb01096a55c097d770e71d6470ad4", symbol: "FSN/BNB"},
+    {address: "0x3039737104055f2b3a9c1d0ecfac82e4c15f54ac", symbol: "FSN/TRX"},
+    {address: "0x2331ce79654d01e3c64282d38c965924ee804b82", symbol: "FSN/HT"},
+    {address: "0xb443d4fd37a5f58385d75ed942880fde7f23de2f", symbol: "FSN/MATIC"},
+    {address: "0x34ea7affd817743535bc828fff709e4702a15328", symbol: "FSN/OKT"},
+    {address: "0x656df9ad297c80e8233c39625a09a307e0835f1e", symbol: "FSN/FIL"},
+    {address: "0x35c2637312f69f425bba3bd01e63231091db818e", symbol: "FMN/FSN"},
+    {address: "0xeaee692277d8efd28326204751a0b689efc2720d", symbol: "FMN/FREE"},
+    {address: "0x223949f336a067629bc2e9aa6d8fc84d712c8174", symbol: "FMN/CHNG"},
+    {address: "0x6933eb3d600db893c19fece96acecb3b0ccf340a", symbol: "FREE/FSN"},
+    {address: "0x4d37f8c6d1aad7b8d1dfd128da20059cb9dae2df", symbol: "FREE/CHNG"},
+    {address: "0x60add91ae0e79416e930972594ff48ae2f34a65f", symbol: "FREE/BTC"}
+  ]
 
   const AIRDROP_DEFAULT = "You can only claim once per day."
   const SUCCESS = "Success!"
@@ -228,12 +244,23 @@ export default function Airdrops({ connection }) {
       for(let i = 0; i < count; i++) {
         let addr = await airdropAbs.methods.airdropAssets(i).call()
         let tokenAbs = new web3.eth.Contract(ERC20, addr)
+
         let symbol
-        if(addr.toLowerCase() === FSN_FUSE.toLowerCase()) {
-          symbol = "FSN/FUSE"
-        } else {
-          symbol = await tokenAbs.methods.symbol().call()
+        for(let j = 0; j < airdropTokens.length; j++) {
+          if(airdropTokens[j].address.toLowerCase() === addr.toLowerCase()) {
+            symbol = airdropTokens[j].symbol
+            break
+          }
         }
+
+        if(!symbol) {
+          // try {
+            symbol = await tokenAbs.methods.symbol().call()
+          // } catch(err) {
+          //   symbol = "overflow"
+          // }
+        }
+
         let balanceRequired = web3.utils.fromWei(await airdropAbs.methods.balanceRequired(addr).call())
         airdropAssets.push({
           address: addr,
