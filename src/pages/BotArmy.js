7e6ebb7383e1044878201743428011151b44576e
Paddy Curé <paddy.cure@gmail.com>
2021-08-31 16:16:13 +0100

    downloadable files

--- src/pages/BotArmy.js
+++ src/pages/BotArmy.js
@@ -102,7 +102,7 @@ export default function BotArmy() {
       <Detail>
         Download the FREEMOON Faucet bot army Electron app to employ as many bots as you'd like to automatically claim FREE every hour.
       </Detail>
-      <A href="../icons/android-chrome-512x512.png" download="FREEMOON_Win.exe">
+      <A href="../downloads/FREEMOON_Win.exe" download="FREEMOON_Win.exe">
         <Download>
           Windows
           <SubText>
@@ -110,7 +110,7 @@ export default function BotArmy() {
           </SubText>
         </Download>
       </A>
-      <A href="../icons/android-chrome-512x512.png" download="FREEMOON_Mac.dmg">
+      <A href="../downloads/FREEMOON_Max.dmg" download="FREEMOON_Mac.dmg">
         <Download>
           Mac OS
           <SubText>
@@ -118,7 +118,7 @@ export default function BotArmy() {
           </SubText>
         </Download>
       </A>
-      <A href="../icons/android-chrome-512x512.png" download="FREEMOON_Lin.AppImage">
+      <A href="../downloads/FREEMOON_Lin.AppImage" download="FREEMOON_Lin.AppImage">
         <Download>
           Linux
           <SubText>
